5e4d13eb7964b676d866af42c6a29de67f52f83a
Levy <leskeylevy@gmail.com>
2018-09-11 14:45:33 +0300

    installed flask mail and imported it in the config.py and also initiated it in init.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from flask_mail import Mail
 
 
 login_manager = LoginManager()
@@ -13,7 +13,7 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-
+mail = Mail
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -23,6 +23,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 export SECRET_KEY=secretkey
+export MAIL_USERNAME=slimleskey@gmail.com
+export MAIL_PASSWORD=D       $
 python3.6 manage.py server
